23336b13da024de7fabc82f3a8dd3c3f35b2c245
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2025-05-05 23:47:54 +0600

    Refactor code structure for improved readability and maintainability

--- registration.cpp
+++ registration.cpp
@@ -4,6 +4,19 @@ using namespace std;
 int main(){
     int n;
     cin >>n;
-
+    unordered_map<string, int>m;
+    string name;
+    for(int i=0; i<n; i++){
+        cin>> name;
+        if(m[name]==0){
+            cout<<"OK"<<endl;
+            m[name]++;
+        }else{
+            cout<<name<<m[name]<<endl;
+            m[name + to_string(m[name])]=1;
+            m[name]++;
+        }
+    }
+    return 0;
     
 }
